2b9bb9c622b53d86064e5f1fdc5860cdf2458723
BALLO-OUSMANE <ousmane.ballo@uvci.edu.ci>
2024-12-18 22:43:34 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Redirige vers la page Marketplace
+function goToMarketplace() {
+    window.location.href = "marketplace.html";
+}
+// Redirige vers la page d'accueil
+function goBack() {
+    window.location.href = "index.html";
+}
+// Redirige vers la page de locaton
+function goTolocation() {
+    window.location.href = "location.html";
+}
+
